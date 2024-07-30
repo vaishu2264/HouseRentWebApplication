228bfeee6b2d4e681a0d6285df32908bf6db0a3a
vaishu2264 <111948064+vaishu2264@users.noreply.github.com>
2024-07-30 23:41:06 +0530

    Update SignUp.jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -71,7 +71,6 @@ export default function SignUp() {
         >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
-        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
