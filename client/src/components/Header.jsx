d8f9a8fccd23e3655a139e7a30df70de7917c31c
vaishu2264 <111948064+vaishu2264@users.noreply.github.com>
2024-07-30 23:37:59 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ export default function Header() {
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>House</span>
-            <span className='text-slate-700'>Rent</span>
+            <span className='text-slate-700'>Hunt</span>
           </h1>
         </Link>
         <form
